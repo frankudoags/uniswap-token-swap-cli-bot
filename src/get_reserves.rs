61f3c460b4e2ed52f8617c76ce8694c7b4822734
Frank_Udoags <frankudoags@gmail.com>
2023-04-16 14:06:19 +0100

    removed unused import

--- src/get_reserves.rs
+++ src/get_reserves.rs
@@ -1,10 +1,6 @@
 use ethers::{
     contract::abigen,
-    core::{
-        types::Address,
-        utils::parse_ether,
-    },
-    
+    core::types::Address,
     providers::{Http, Provider},
 };
 use eyre::Result;
@@ -40,18 +36,26 @@ pub async fn get_reserves(pool: Address) -> Result<()> {
     // instantiate the pair contract bindings
     let pair = IUniswapV2Pair::new(pool, Arc::clone(&client));
     let (reserve0, reserve1, _timestamp) = pair.get_reserves().call().await?;
-    
-    
+
     // get the reserves
     // get the token addresses
     let token0 = pair.token_0().call().await?;
     let token1 = pair.token_1().call().await?;
     // get the token names from the token addresses
-    let token0_name = IERC20::new(token0, Arc::clone(&client)).name().call().await?;
-    let token1_name = IERC20::new(token1, Arc::clone(&client)).name().call().await?;
+    let token0_name = IERC20::new(token0, Arc::clone(&client))
+        .name()
+        .call()
+        .await?;
+    let token1_name = IERC20::new(token1, Arc::clone(&client))
+        .name()
+        .call()
+        .await?;
 
     // print the reserves with the token names
-    println!("Reserves for {} and {}: {} and {}", token0_name, token1_name, reserve0, reserve1);
+    println!(
+        "Reserves for {} and {}: {} and {}",
+        token0_name, token1_name, reserve0, reserve1
+    );
     // calculate the mid price, no idea if this is correct, copied from ethers-rs examples: ethers-rs/examples/queries/examples/uniswapv2_pair.rs
     let mid_price = f64::powi(10.0, 18 - 6) * reserve1 as f64 / reserve0 as f64;
     println!("Price of {} in {}: {}", token0_name, token1_name, mid_price);
